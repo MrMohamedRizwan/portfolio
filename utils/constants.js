72aa55f0909b585f8a33ea449a5e52005421efce
MrMohamedRizwan <109430288+MrMohamedRizwan@users.noreply.github.com>
2024-02-23 15:13:22 +0530

    git

--- utils/constants.js
+++ utils/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Disposable email finder",
     overview: "A website which is used to find disposable emails it is done by calling API's and by checking whether the given mail is a disposable email or not and this is used to avoid a lot of spam networks",
     stack: ["Html", "CSS", "Python"],
-    repo: "https://github.com/danielcranney/yodlr",
+    repo: "https://github.com/MrMohamedRizwan",
     isSiteLive: true,
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
       "This project is used to generate energy during night from solar panels, These solar panels are placed on the divider of highways and are rotated 90 deegrees during night a web application is developed to monitor these solar panels",
     stack: ["Html", "CSS", "Embedded C",],
     // link: "http://yodlr.vercel.app",
-    repo: "https://github.com/danielcranney/yodlr",
+    repo: "https://github.com/MrMohamedRizwan",
     isSiteLive: true,
   },
   {
@@ -21,7 +21,7 @@ export const projects = [
       "This is a samrt switch which is developed using IOT which can also be controlled by mobile application this smart switch can be used in various locations and can be used to save energy",
     stack: ["Embedded C"],
     // link: "http://www.ratemyfilm.co.",
-    repo: "https://github.com/danielcranney/rate-my-film",
+    repo: "https://github.com/MrMohamedRizwan",
     isSiteLive: true,
   },
   
